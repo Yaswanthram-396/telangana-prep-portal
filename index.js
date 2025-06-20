3652cc113ace6f4693c140b1c0571866071cccd3
Yaswanthram-396 <yaswanthram.396@gmail.com>
2025-06-21 04:17:42 +0530

    with crid

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: 'http://localhost:8080',
+  withCredentials: true,
 }));
 app.use(express.json());
 app.use("/api", loginRouts);
