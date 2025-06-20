ca5c1e2b521e7d9e89e69b979c2bc669b8f38ff9
Yaswanthram-396 <yaswanthram.396@gmail.com>
2025-06-21 04:08:49 +0530

    added serve

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:8080',
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api", loginRouts);
 app.use("/api/ask-ai",askai);
